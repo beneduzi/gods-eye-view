34bdfb8219758c839604ff69d054a1c3f0d3bf40
Gustavo Beneduzi <gasbeneduzi@gmail.com>
2026-09-15 08:37:04 -0300

    test: update cloud layer catalog counts

--- src/data/layerState.js
+++ src/data/layerState.js
@@ -319,8 +319,8 @@ export const LAYER_STATE_REGISTRY = Object.freeze([
     disposition: 'enabled+options',
     optionOwner: 'cctv',
   }),
-  Object.freeze({ id: 'directions', token: 'n', disposition: 'enabled-only' }),
   Object.freeze({ id: 'clouds', token: 'h', disposition: 'enabled-only' }),
+  Object.freeze({ id: 'directions', token: 'n', disposition: 'enabled-only' }),
   Object.freeze({ id: 'earthquakes', token: 'e', disposition: 'enabled-only' }),
   Object.freeze({
     id: 'flights',
